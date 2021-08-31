91bcffede5b6d645d0357b406b5e609d10c82274
u-shylianok <u.shylianok@gmail.com>
2021-09-01 00:55:59 +0300

    Add main photo

--- task05/internal/model/photo.go
+++ task05/internal/model/photo.go
@@ -1,7 +1,8 @@
 package model
 
 type Photo struct {
-	Id   int    `json:"id"`
-	AdId int    `json:"ad_id"`
-	Link string `json:"link"`
+	Id     int    `json:"id"`
+	AdId   int    `json:"ad_id"`
+	IsMain bool   `json:"is_main"`
+	Link   string `json:"link"`
 }
